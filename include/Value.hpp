6a0f3558cab4ce17b2dc076234a081ceee4832bf
Leon Cheung <leonjcheung@gmail.com>
2026-03-29 16:45:32 -0400

    merge create-value

--- include/Value.hpp
+++ include/Value.hpp
@@ -1,8 +1,41 @@
 #pragma once
 
+#include <memory>
+#include <unordered_set>
+#include <vector>
+
 namespace mg {
-class Value {
+class Value : public std::enable_shared_from_this<Value> {
 public:
-  Value();
+  Value(double data, std::vector<std::shared_ptr<Value>> children = {},
+        std::vector<double> local_grads = {});
+
+  void backward();
+
+  std::shared_ptr<Value> pow(double value);
+  std::shared_ptr<Value> log();
+  std::shared_ptr<Value> exp();
+  std::shared_ptr<Value> relu();
+
+  std::shared_ptr<Value> neg();
+
+private:
+  double data;
+  double grad;
+
+  // maybe could be unique_ptr.. children cannot be shared right?
+  std::vector<std::shared_ptr<Value>> children;
+  std::vector<double> local_grads;
+
+  std::shared_ptr<Value> add(const std::shared_ptr<Value> &other);
+  std::shared_ptr<Value> add(double value);
+  std::shared_ptr<Value> sub(const std::shared_ptr<Value> &other);
+  std::shared_ptr<Value> mul(const std::shared_ptr<Value> &other);
+  std::shared_ptr<Value> mul(double value);
+  std::shared_ptr<Value> div(const std::shared_ptr<Value> &other);
+
+  void build_topo(const std::shared_ptr<Value> &v,
+                  std::vector<std::shared_ptr<Value>> &topo,
+                  std::unordered_set<std::shared_ptr<Value>> &visited);
 };
 } // namespace mg
--- src/Value.cpp
+++ src/Value.cpp
@@ -1,6 +1,93 @@
 #include "Value.hpp"
 
-namespace mg {
-Value::Value() {}
+#include <cassert>
+#include <ranges>
+#include <unordered_set>
 
+namespace mg {
+Value::Value(double data, std::vector<std::shared_ptr<Value>> children,
+             std::vector<double> local_grads)
+    : data(data), grad(0), children(std::move(children)),
+      local_grads(std::move(local_grads)) {}
+
+void Value::backward() {
+  std::vector<std::shared_ptr<Value>> topo;
+  std::unordered_set<std::shared_ptr<Value>> visited;
+  build_topo(shared_from_this(), topo, visited);
+
+  grad = 1.0;
+
+  for (const auto &value : std::views::reverse(topo)) {
+    const std::vector<std::shared_ptr<Value>> &c = value->children;
+    const std::vector<double> &lg = value->local_grads;
+    assert(c.size() == lg.size());
+
+    for (size_t i = 0; i < c.size(); ++i) {
+      c.at(i)->grad += lg.at(i) * value->grad;
+    }
+  }
+}
+
+void Value::build_topo(const std::shared_ptr<Value> &v,
+                       std::vector<std::shared_ptr<Value>> &topo,
+                       std::unordered_set<std::shared_ptr<Value>> &visited) {
+  if (visited.contains(v)) {
+    return;
+  }
+  visited.insert(v);
+  for (const auto &child : v->children) {
+    build_topo(child, topo, visited);
+  }
+  topo.push_back(v);
+}
+
+std::shared_ptr<Value> Value::add(const std::shared_ptr<Value> &other) {
+  return std::make_shared<Value>(
+      data + other->data,
+      std::vector<std::shared_ptr<Value>>{shared_from_this(), other},
+      std::vector<double>{1.0, 1.0});
+}
+std::shared_ptr<Value> Value::add(double value) {
+  return add(std::make_shared<Value>(value));
+}
+std::shared_ptr<Value> Value::sub(const std::shared_ptr<Value> &other) {
+  return add(other->neg());
+}
+std::shared_ptr<Value> Value::mul(const std::shared_ptr<Value> &other) {
+  return std::make_shared<Value>(
+      data * other->data,
+      std::vector<std::shared_ptr<Value>>{shared_from_this(), other},
+      std::vector<double>{other->data, data});
+}
+std::shared_ptr<Value> Value::mul(double value) {
+  return mul(std::make_shared<Value>(value));
+}
+std::shared_ptr<Value> Value::div(const std::shared_ptr<Value> &other) {
+  return mul(other->pow(-1.0));
+}
+
+std::shared_ptr<Value> Value::pow(double value) {
+  return std::make_shared<Value>(
+      std::pow(data, value),
+      std::vector<std::shared_ptr<Value>>{shared_from_this()},
+      std::vector<double>{value * std::pow(data, value - 1)});
+}
+std::shared_ptr<Value> Value::log() {
+  return std::make_shared<Value>(
+      std::log(data), std::vector<std::shared_ptr<Value>>{shared_from_this()},
+      std::vector<double>{1.0 / data});
+}
+std::shared_ptr<Value> Value::exp() {
+  return std::make_shared<Value>(
+      std::exp(data), std::vector<std::shared_ptr<Value>>{shared_from_this()},
+      std::vector<double>{std::exp(data)});
+}
+std::shared_ptr<Value> Value::relu() {
+  return std::make_shared<Value>(
+      std::max(0.0, data),
+      std::vector<std::shared_ptr<Value>>{shared_from_this()},
+      std::vector<double>{data > 0.0 ? 1.0 : 0.0});
+}
+
+std::shared_ptr<Value> Value::neg() { return mul(-1.0); }
 } // namespace mg
